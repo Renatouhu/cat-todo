d661de6465234faa2fa6879d7f20a327d29a5a3c
Renatouhu <renatoalves649@gmail.com>
2024-02-10 13:22:47 -0300

    Added the rest of themes as buttons on change theme container

--- src/app/components/header/header.jsx
+++ src/app/components/header/header.jsx
@@ -7,16 +7,16 @@ import { currentTheme } from "../../../../public/themes/themes";
 
 export const Header = (props) => {
   function onClickThemeIcon() {
-    const themeContainer = document.getElementById('themeContainer');
+    const themeContainer = document.getElementById("themeContainer");
     const isHidden = themeContainer.classList.contains(
       styles.hiddenThemeContainer
     );
     if (isHidden) {
       themeContainer.classList.remove(styles.hiddenThemeContainer);
-      themeContainer.classList.add(styles.ThemeContainer);
+      themeContainer.classList.add(styles.themeContainer);
       return;
     }
-    themeContainer.classList.remove(styles.ThemeContainer);
+    themeContainer.classList.remove(styles.themeContainer);
     themeContainer.classList.add(styles.hiddenThemeContainer);
   }
 
@@ -28,7 +28,13 @@ export const Header = (props) => {
       <div className={styles.logo}>
         <div className={styles.logoIcon}>
           {props.withimage === "true" ? (
-            <Image src={Logo} alt="a draw of a cat" width={158} height={128} />
+            <Image
+              src={Logo}
+              alt="a draw of a cat"
+              width={158}
+              height={128}
+              priority={true}
+            />
           ) : (
             ""
           )}
@@ -48,17 +54,39 @@ export const Header = (props) => {
         <div
           id="themeContainer"
           className={styles.hiddenThemeContainer}
-          style={{ backgroundColor: currentTheme.colors.primary }}
+          style={{ backgroundColor: currentTheme.colors.primaryContainer }}
         >
           <button
-            style={{ backgroundColor: currentTheme.colors.onPrimaryContainer }}
+            style={{
+              background: "linear-gradient(to left, #fef7ff 50%, #6750A4 50%)",
+              color: "black",
+            }}
           >
-            Tema Escuro
+            Light
           </button>
           <button
-            style={{ backgroundColor: currentTheme.colors.onPrimaryContainer }}
+            style={{
+              background: "linear-gradient(to left, #1d1b20 50%, #49454f 50%)",
+              color: "white",
+            }}
           >
-            Tema Roxo Pastel
+            Dark
+          </button>
+          <button
+            style={{
+              background: "linear-gradient(to left, #330F18 50%, #E6A4B4 50%)",
+              color: "white",
+            }}
+          >
+            Red Pastel
+          </button>
+          <button
+            style={{
+              background: "linear-gradient(to left, #3D2661 50%, #705794 50%)",
+              color: "white",
+            }}
+          >
+            Purple Pastel
           </button>
         </div>
       </div>
